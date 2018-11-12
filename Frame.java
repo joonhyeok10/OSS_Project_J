2b42761ae8be9cca3cdf063dfb6c784c143ebb2e
20144804 <43734316+20144804@users.noreply.github.com>
2018-11-13 01:29:44 +0900

    Add files via upload
    스윙 프레임 생성 예제입니다.

---
+++ Frame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	public Frame() {
+		setTitle("프레임 생성");//제목입력
+		setSize(500,500);//사이즈 입력
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame f=new Frame();
+	}
+
+}
