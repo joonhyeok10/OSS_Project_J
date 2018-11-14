152b84e872618d43f7ce770dafc3a8dd4c60d328
yuujung <44626412+yuujung@users.noreply.github.com>
2018-11-14 10:14:41 +0900

    자바_9장예제

---
+++ MyFrame.java
@@ -0,0 +1,19 @@
+package Package01;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+	public MyFrame() {
+		setTitle("300*300스윙 프레임 만들기");
+		setSize(300,300);
+		setVisible(true);		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFrame frame= new MyFrame();
+	}
+
+}
+
